46bb05250ef7d0f9af6b55514819b4e162693076
Youssef <74077927+usefitsoftware@users.noreply.github.com>
2023-02-01 19:46:09 +0100

    Update medir.py

--- medir.py
+++ medir.py
@@ -1,29 +1,31 @@
 
-import Adafruit_DHT
+import time
+import adafruit_dht
 
-sensor = Adafruit_DHT.DHT22
-pin = 4
+dhtDevice = adafruit_dht.DHT22(4)
 
 while True:
-  humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
-  if temperature is not None:
-    if temperature < 5:
-      print("Temperatura menor de 5 grados")
-    elif temperature >= 5 and temperature < 10:
-      print("Temperatura mayor de 5 y menor de 10")
-    elif temperature >= 10 and temperature < 15:
-      print("Temperatura mayor de 10 y menor de 15")
-    elif temperature >= 15 and temperature < 20:
-      print("Temperatura mayor de 15 y menor de 20")
-    elif temperature >= 20 and temperature < 25:
-      print("Temperatura mayor de 20 y menor de 25")
-    elif temperature >= 25 and temperature < 30:
-      print("Temperatura mayor de 25 y menor de 30")
-    elif temperature >= 30 and temperature < 35:
-      print("Temperatura mayor de 30 y menor de 35")
-    elif temperature >= 35 and temperature < 40:
-      print("Temperatura mayor de 35 y menor de 40")
-    else:
-      print("Temperatura mayor de 40")
-  else:
-    print("Error al obtener la temperatura")
+    try:
+        t = dhtDevice.temperature
+        if t < 5:
+            print("Atención es posible riesgo de helada o nieve, temperatura muy baja({:.1f}°C)".format(t))
+        elif t >= 5 and t < 10:
+            print("En estos momentos hace frío, temperatura baja de ({:.1f}°C)".format(t))
+        elif t >= 10and t < 15:
+            print("Ahora mismo se nota cierta bajada de termometro, hay ({:.1f}°C)".format(t))
+        elif t >= 15 and t < 20:
+            print("La temperatura actual es agradable de ({:.1f}°C)".format(t))
+        elif t >= 20 and t < 26:
+            print("En estos moemntos hay una temperatura fantástica y primaveral, exactamente ({:.1f}°C)".format(t))
+        elif t >= 26 and t < 31:
+            print("En estos moentos la temperatura es alta, hay ({:.1f}°C)".format(t))
+        elif t >= 31 and t < 36:
+            print("La temperatura ha subido mucho, es necesario tomar precaución, hay ({:.1f}°C)".format(t))
+        elif t >= 36 and t < 40:
+            print("La tempera actual es elevada y se considera cierto riesgo para la salud, tenga cuidado, hay exactamente({:.1f}°C)".format(t))
+        else:
+            print("Atención,PELIGRO!!!la tmepoeratura actual es demasiado elevada, se recomienda no salir al exterior, hay ({:.1f}°C)".format(t))
+    except RuntimeError as error:
+        print(error.args[0])
+    time.sleep(2.0)
+
