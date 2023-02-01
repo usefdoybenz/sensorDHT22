73eebb46bbf9ab14f8fb25a301afd4ea92e79c8e
Youssef <74077927+usefitsoftware@users.noreply.github.com>
2023-02-01 22:37:24 +0100

    Update medir.py

--- medir.py
+++ medir.py
@@ -7,25 +7,27 @@ dhtDevice = adafruit_dht.DHT22(4)
 while True:
     try:
         t = dhtDevice.temperature
+
         if t < 5:
-            print("Atención es posible riesgo de helada o nieve, temperatura muy baja({:.1f}°C)".format(t))
+            print("Hay demasiado frio y peude haber heladas o nievem, estamos a ({:.1f}C)".format(t))
         elif t >= 5 and t < 10:
-            print("En estos momentos hace frío, temperatura baja de ({:.1f}°C)".format(t))
+            print("En estos momentos hace frio, temperaturas baja de ({:.1f}C)".format(t))
         elif t >= 10and t < 15:
-            print("Ahora mismo se nota cierta bajada de termometro, hay ({:.1f}°C)".format(t))
+            print("Ahora mismo se las temperaturas son algo bajas pero no hace mucho frio, hay ({:.1f}C)".format(t))
         elif t >= 15 and t < 20:
-            print("La temperatura actual es agradable de ({:.1f}°C)".format(t))
+            print("La temperatura actual es estable, no hace frio ni tampoco mucho calor, hay ({:.1f}C)".format(t))
         elif t >= 20 and t < 26:
-            print("En estos moemntos hay una temperatura fantástica y primaveral, exactamente ({:.1f}°C)".format(t))
+            print("En estos momemntos hay una temperatura agradable, exactamente ({:.1f}C)".format(t))
         elif t >= 26 and t < 31:
-            print("En estos moentos la temperatura es alta, hay ({:.1f}°C)".format(t))
+            print("Segun los datos la temperatura es alta pero sin riesgo, hay ({:.1f}C)".format(t))
         elif t >= 31 and t < 36:
-            print("La temperatura ha subido mucho, es necesario tomar precaución, hay ({:.1f}°C)".format(t))
+            print("La temperatura es elevada,hay ({:.1f}°C)".format(t))
         elif t >= 36 and t < 40:
-            print("La tempera actual es elevada y se considera cierto riesgo para la salud, tenga cuidado, hay exactamente({:.1f}°C)".format(t))
+            print("Hace demasiado calor, hay exactamente({:.1f}C)".format(t))
         else:
-            print("Atención,PELIGRO!!!la tmepoeratura actual es demasiado elevada, se recomienda no salir al exterior, hay ({:.1f}°C)".format(t))
+            print("Atencion,PELIGRO!!!la tmeperatura actual es un riesgo importante para la salaud y el entorno, se recomienda no salir al exterior, hay ({:.1f}C)".f$
     except RuntimeError as error:
         print(error.args[0])
     time.sleep(2.0)
+
 
